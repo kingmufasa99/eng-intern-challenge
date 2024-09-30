ac811496d95bb20a788dc55c5714e9d58c4573d6
Moulay-Mostafa Filali <moulay-mostafa.filali.1@ulaval.ca>
2024-09-30 16:23:54 -0400

    F - defining AlphaToBraille(input) with basic dictionary

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,62 @@
+import sys
+
+brailleDict = {
+    'a': 'O.....',
+    'b': 'O.O...',
+    'c': 'OO....',
+    'd': 'OO.O..',
+    'e': 'O..O..',
+    'f': 'OOO...',
+    'g': 'OOOO..',
+    'h': 'O.OO..',
+    'i': '.OO...',
+    'j': '.OOO..',
+    'k': 'O...O.',
+    'l': 'O.O.O.',
+    'm': 'OO..O.',
+    'n': 'OO.OO.',
+    'o': 'O..OO.',
+    'p': 'OOO.O.',
+    'q': 'OOOOO.',
+    'r': 'O.OOO.',
+    's': '.OO.O.',
+    't': '.OOOO.',
+    'u': 'O...OO',
+    'v': 'O.O.OO',
+    'w': '.OOO.O',
+    'x': 'OO..OO',
+    'y': 'OO.OOO',
+    'z': 'O..OOO',
+    '1': 'O.....',
+    '2': 'O.O...',
+    '3': 'OO....',
+    '4': 'OO.O..',
+    '5': 'O..O..',
+    '6': 'OOO...',
+    '7': 'OOOO..',
+    '8': 'O.OO..',
+    '9': '.OO...',
+    '0': '.OOO..',
+    ' ': '......'
+}
+
+def AlphaToBraille(input):
+
+    output = ""
+    for c in input:
+        if c.lower() in brailleDict:
+            if c.isupper():
+                output += ".....O" + brailleDict[c.lower()]
+            else:
+                output += brailleDict[c]
+    return output
+
+
+if __name__ == "__main__":
+    input_string = " ".join(sys.argv[1:])
+
+    braille_output = AlphaToBraille(input_string)
+
+    print(braille_output)
+
 
